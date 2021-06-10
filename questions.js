eab0da45e6280b6ac04989e0f5fbce9a9536ef98
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-06-10 10:52:59 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,56 @@
+//Array of objects
+const quiz = [
+    {
+        q:"____'s your name? - Thomas.",
+        options:["What", "How", "Who", "Where"],
+        answer: 0
+    },
+    {
+        q:"This is Lucy and her brother Dan. ____ my friends.",
+        options:["We're", "They're", "You're", "I'm"],
+        answer: 1
+    },
+    {
+        q:"____ - I'm from Italy.",
+        options:["Where from you are?", "Where you are from?", "Where are you from?", "From where you are?"],
+        answer: 2
+    },
+    {
+        q:"I'm from Milan. ____ is in Italy.",
+        options:["She", "He", "They", "It" ],
+        answer: 3
+    },
+    {
+        q:"Excuse me, how ____ your last name? - R-I-L-E-Y.",
+        options:["do you spell", "spell", "you spell", "spell you"],
+        answer: 0
+    },
+    {
+        q:"Oh, ____ are my keys!",
+        options:["it", "these", "this", "that"],
+        answer: 1
+    },
+    {
+        q:"I'd like ____ omelette, please.",
+        options:["a", "-", "an", "two"],
+        answer: 2   
+    },
+    {
+        q:"I'm Jeff Caine. Nice to ____ you, Mr Caine.",
+        options:["speak", "talk", "watch", "meet"],
+        answer: 3
+    },
+    {
+        q:"Can I help you? Thanks, but I'm just ____ .",
+        options:["looking", "watching", "seeing", "shopping"],
+        answer: 0
+    },
+    {
+        q:"Mandy is over there. She's ____ a blue T-shirt and jeans.",
+        options:["having", "wearing", "doing", "walking"],
+        answer: 1
+    }
+    
+
+
+]
